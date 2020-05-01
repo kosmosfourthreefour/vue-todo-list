27ab1762ff25fe08a50e5a545e79da35b14e89b4
kosmos434 <kosmos434@protonmail.com>
2020-05-01 16:21:43 -0500

    add Task.vue and Tasklist.vue

---
+++ src/components/Task.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="todo-item" v-bind:class="{'is-complete':aTask.completed}">
+        <p>
+            <input type="checkbox" v-on:click="markComplete">
+            {{aTask.title}}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Task",
+    props: ['aTask'],
+    methods:
+    {
+        markComplete(){
+            this.aTask.completed = !this.aTask.completed;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .todo-item
+    {
+        background: #f4f4f4;
+        border-bottom: 1px #ccc dotted;
+        padding: 10px;
+    }
+
+    .is-complete
+    {
+        text-decoration: line-through;
+    }
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="taskDiv">
+        <p v-for="todo in todos" v-bind:key="todo.id">
+            <Task v-bind:aTask="todo"/>
+        </p>
+    </div>
+</template>
+
+<script>
+import Task from '@/components/Task.vue';
+
+
+export default {
+    name: "TaskList",
+    props: ['todos'],
+    components: {
+    Task,
+  },
+}
+</script>
+
+<style scoped>
+    .taskDiv 
+    {
+        font-family: Arial;
+        font-size: 35px;
+    }
+</style>
